dc521358b772e134f9e144e727a17fa3254992c3
dahn0209 <dahn0209@gmail.com>
2021-03-10 12:36:53 -0500

    added background image

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -11,30 +11,38 @@ class AllProducts extends React.Component {
   render() {
     const products = this.props.products
     return (
-      <div>
-        <h1>All Products</h1>
-        <h2>List of Products:</h2>
-        <div id="all-products-wrapper">
-          {products.map(product => {
-            return (
-              <div className="all-product-list" key={product.id}>
-                <div className="all-product-item">
-                  <Link
-                    className="product-name-image"
-                    to={`/products/${product.id}`}
-                  >
-                    <h2>{product.name}</h2>
-                    <img src={product.imageUrl} />
-                  </Link>
-                  <p>{product.description}</p>
-                  <h3>Price: {product.price}</h3>
-                </div>
-              </div>
-            )
-          })}
+      <section id="background-pic">
+        <div>
+          <div id="image-txt">
+            <h1>All Products</h1>
+            <h2>List of Products:</h2>
+            <div id="all-products-wrapper">
+              {products.map(product => {
+                return (
+                  <div className="all-product-list" key={product.id}>
+                    <div className="all-product-item">
+                      <Link
+                        className="product-name-image"
+                        to={`/products/${product.id}`}
+                      >
+                        <h2>{product.name}</h2>
+                        <img src={product.imageUrl} />
+                      </Link>
+                      <p>{product.description}</p>
+                      <h3>Price: {product.price}</h3>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            <div />
+          </div>
+          <img
+            className="backgroundIMG"
+            src="https://images6.alphacoders.com/992/992445.jpg"
+          />
         </div>
-        <div />
-      </div>
+      </section>
     )
   }
 }
